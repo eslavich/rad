c5dee41a4defd30810392ee63ebbc472e793b669
Ed Slavich <eslavich@stsci.edu>
2021-08-26 12:49:15 -0400

    Include write_to as well

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ from setuptools import setup
 
 # This should be enabled by pyproject.toml, but that doesn't seem
 # to work with pip 21.2.4.
-setup(use_scm_version=True)
+setup(
+    use_scm_version=True,
+    write_to="src/rad/_version.py",
+)
