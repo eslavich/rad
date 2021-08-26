f6a4a6646ec7dc9f1280bf8773d8606c126785e6
Ed Slavich <eslavich@stsci.edu>
2021-08-26 12:51:19 -0400

    Oops

--- setup.py
+++ setup.py
@@ -3,7 +3,4 @@ from setuptools import setup
 
 # This should be enabled by pyproject.toml, but that doesn't seem
 # to work with pip 21.2.4.
-setup(
-    use_scm_version=True,
-    write_to="src/rad/_version.py",
-)
+setup(use_scm_version={"write_to": "src/rad/_version.py"})
